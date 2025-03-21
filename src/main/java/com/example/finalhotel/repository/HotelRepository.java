51d53d00ec3c5e0358688d2ba20b41c524535579
segong <sy981022@naver.com>
2025-03-21 14:54:00 +0900

    등록 서비스중

--- src/main/java/com/example/finalhotel/repository/HotelRepository.java
+++ src/main/java/com/example/finalhotel/repository/HotelRepository.java
@@ -3,11 +3,11 @@ package com.example.finalhotel.repository;
 import com.example.finalhotel.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
-    //본사 이름(brandTitle)을 입력하면 본사의 pk를 찾아와서 Hotel에 넣어준다.
-   public Hotel findByBrand_BrandTitle(String brandTitle);
-    //+++ 이건 본사에 넣어야 하는 메서드인가?
-
+    //본사(총판) pk 받으면 그 본사(총판)이 가지고있는 Hotel을 보여준다.
+    public List<Hotel> findByBrand_BrandNum(Long brandNum);
 
 }
---
+++ src/main/java/com/example/finalhotel/service/HotelService.java
@@ -0,0 +1,21 @@
+package com.example.finalhotel.service;
+
+import com.example.finalhotel.dto.HotelDTO;
+
+public interface HotelService {
+
+    //호텔 등록
+    public HotelDTO hotelInsert(HotelDTO hotelDTO, Long brandNum);
+
+    //호텔 목록
+
+    //호텔 상세보기
+
+    //호텔 수정
+
+    //호텔 삭제
+
+
+
+
+}
