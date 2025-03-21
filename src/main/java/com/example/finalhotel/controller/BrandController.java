e34cd2f33cb3eacc2d9bea6597ec6cbaed4bf5f4
yungyeom <kyg6389@naver.com>
2025-03-21 12:36:12 +0900

    기본 베이스

--- src/main/java/com/example/finalhotel/controller/BrandController.java
+++ src/main/java/com/example/finalhotel/controller/BrandController.java
@@ -1,11 +1,13 @@
 package com.example.finalhotel.controller;
 
 import com.example.finalhotel.dto.BrandDTO;
+import com.example.finalhotel.repository.BrandRepository;
 import com.example.finalhotel.service.BrandService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Log4j2
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BrandController {
 
     private final BrandService brandService;
+    private final BrandRepository brandRepository;
 
     @GetMapping("/brand/insert")
     public String insertGet(BrandDTO brandDTO){
@@ -22,11 +25,14 @@ public class BrandController {
         return "brand/insert";
     }
 
-    @GetMapping("/brand/insert")
+    @PostMapping("/brand/insert")
     public String insertPost(BrandDTO brandDTO){
         log.info("post 방식 본사 가입 컨트롤러 진입" + brandDTO);
 
+        brandDTO =
+                brandService.insert(brandDTO);
 
+        log.info("저장된 brandDTO" + brandDTO);
 
         return "brand/insert";
     }
