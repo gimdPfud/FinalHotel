ff1d7dd1142863ec53ad624d426cecbd7a73e8be
예령 <gimdPfud@gmail.com>
2025-03-21 11:44:53 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/finalhotel/FinalHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalhotel/FinalHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
