d6843e88282f1fb1e61f6cb30af74293f0288b6d
segong <sy981022@naver.com>
2025-03-21 16:42:21 +0900

    4시 42분

--- src/main/java/com/example/finalhotel/controller/HotelController.java
+++ src/main/java/com/example/finalhotel/controller/HotelController.java
@@ -6,7 +6,9 @@ import com.example.finalhotel.service.HotelService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,12 +22,31 @@ public class HotelController {
     private final HotelService hotelService;
 
     @GetMapping("/list")
-    public String getList(HotelDTO hotelDTO) {
+    public String getList(Long brandNum, Model model) {
         log.info("list getList Controller 진입");
-        hotelService.hotelInsert(hotelDTO);
+        List<HotelDTO> hotelDTOS = hotelService.hotelList(brandNum);
+        model.addAttribute("hotelDTOS", hotelDTOS);
 
         return "hotel/list";
     }
+
+    @GetMapping("/insert")
+    public String getInsert(){
+        log.info("list getInsert Controller 진입");
+
+        return "hotel/insert";
+    }
+
+    @PostMapping("/insert")
+    public String postInsert(HotelDTO hotelDTO){
+        log.info("list postInsert Controller 진입");
+
+        hotelService.hotelInsert(hotelDTO);
+
+        return "redirect:/hotel/list";
+    }
+
+
 
 
 
