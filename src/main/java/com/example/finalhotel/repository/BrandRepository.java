8f1b0a521e35fecb7a5078a57786c61ae115cb3a
yungyeom <kyg6389@naver.com>
2025-03-21 14:28:05 +0900

    레포지토리2

--- src/main/java/com/example/finalhotel/repository/BrandRepository.java
+++ src/main/java/com/example/finalhotel/repository/BrandRepository.java
@@ -3,7 +3,10 @@ package com.example.finalhotel.repository;
 import com.example.finalhotel.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
-Brand findByMember_MemberEmail(String Email);
+
+List<Brand> findByMember_MemberEmail(String Email);
 
 }
