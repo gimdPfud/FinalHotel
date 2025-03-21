6b641fdfc2d93fb9656f4c8b9a47c3a2e8270120
segong <sy981022@naver.com>
2025-03-21 12:00:44 +0900

    Hotel entity, DTO, Repository 작성 완

---
+++ src/main/java/com/example/finalhotel/repository/HotelRepository.java
@@ -0,0 +1,11 @@
+package com.example.finalhotel.repository;
+
+import com.example.finalhotel.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
+
+
+
+}
